5c96927838f52ef74285e6c09177e320c5602cb6
landsprutte <christianbonesen@gmail.com>
2024-07-04 12:22:51 +0200

    max and min for minutes and hours

--- src/server/timesheet/validation.ts
+++ src/server/timesheet/validation.ts
@@ -2,8 +2,8 @@ import { z } from "zod";
 
 export const timesheetSchema = z.object({
   date: z.string(),
-  hours: z.coerce.number(),
-  minutes: z.coerce.number(),
+  hours: z.coerce.number().max(24),
+  minutes: z.coerce.number().max(60),
   description: z?.string().max(548),
   id: z.number().optional(),
 });
